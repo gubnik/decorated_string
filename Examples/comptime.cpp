58496b261d1e1d3e47df6bc90ab01dbd1a58bb4f
Nikolay Gubankov <nikgub@cock.li>
2024-05-01 03:59:58 +0300

    Improved examples

--- Examples/comptime.cpp
+++ Examples/comptime.cpp
@@ -1,8 +1,18 @@
 #include "../decstr_comptime.hpp"
 
+#include <iostream>
+
+std::ostream& operator<< (std::ostream& out, const std::array<char, 8>& arr)
+{
+    for (auto& it : arr) out << it;
+    return out;
+}
+
 int main (int argc, char ** argv)
 {
-    constexpr std::array<char, 8> arr = decstr::make_decorator_array
-        <decstr::RED, decstr::DEFAULT, decstr::NONE>();
-        //= {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    // Generates an array of chars that contain a decorator
+    constexpr std::array<char, 8> arr = decstr::make_decorator_array<decstr::RED, decstr::DEFAULT, decstr::NONE>();
+    
+    // Outputs red "Hello git!" string
+    std::cout << arr << "Hello git!\n" << "\033[m";
 }
