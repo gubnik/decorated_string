bb1e4661cde8c4861b37b58e83cba8496685e3cc
Nikolay Gubankov <nikgub@cock.li>
2024-05-01 04:45:11 +0300

    Added 'make install', updated examples to match it

--- Examples/colours.cpp
+++ Examples/colours.cpp
@@ -1,9 +1,13 @@
-#include "../decstr_def.hpp"
+#include <decstr_def.hpp>
+#include <iostream>
 
 int main (int argc, char ** argv)
 {
     // Makes a red string
     std::string S1 = decstr::make_decorated("Balls", decstr::RED);
-    // Makes a bold blue string with high text intensity
-    std::string S2 = decstr::make_decorated("Balls", decstr::BLUE, decstr::TEXT, decstr::BOLD);
+    std::cout << S1 << '\n';
+
+    // Makes a bold blue string with high background intensity
+    std::string S2 = decstr::make_decorated("Balls", decstr::BLUE, decstr::TEXT, decstr::UNDERLINED);
+    std::cout << S2 << '\n';
 }
